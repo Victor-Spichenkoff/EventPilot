303728ffb5461bd5eb4e734b3943c0f45ba0b7d2
Victor-Spichenkoff <victorspich13@gmail.com>
2026-01-24 13:15:07 -0300

    Auto creating and migrating db

--- src/EventPilot.Infrastructure/Seed/RunMigration.cs
+++ src/EventPilot.Infrastructure/Seed/RunMigration.cs
@@ -6,29 +6,29 @@ using Microsoft.Extensions.Logging;
 
 namespace EventPilot.Infrastructure.Seed;
 
-// public static class RunMigration
-// {
-//     public static void RunMigrations(this WebApplication app)
-//     {
-//         using (var scope = app.Services.CreateScope())
-//         {
-//             var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//
-//             try
-//             {
-//                 db.Database.Migrate();
-//                 app.Logger.LogInformation(
-//                     new EventId(1001, "StartupApp"),
-//                     "Migrating database done");
-//             }
-//             catch (Exception ex)
-//             {
-//                 app.Logger.LogError(new EventId(1001, "StartupApp"),
-//                     "Migrating database NOT done");
-//                 Console.WriteLine(ex);
-//                 throw;
-//             }
-//         }
-//
-//     }
-// }
+public static class RunMigration
+{
+    public static void RunMigrations(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            try
+            {
+                db.Database.Migrate();
+                app.Logger.LogInformation(
+                    new EventId(1001, "StartupApp"),
+                    "Migrating database done");
+            }
+            catch (Exception ex)
+            {
+                app.Logger.LogError(new EventId(1001, "StartupApp"),
+                    "Migrating database NOT done");
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+    }
+}
