7366c3cfec4c7909fcc40ee016b9a4c60b58a497
Victor-Spichenkoff <victorspich13@gmail.com>
2026-01-27 09:39:29 -0300

    Swagger with auth working

--- src/EventPilot.API/Extensions/AddSwagger.cs
+++ src/EventPilot.API/Extensions/AddSwagger.cs
@@ -1,4 +1,5 @@
-using Microsoft.OpenApi;
+
+using Microsoft.OpenApi.Models;
 
 namespace EventPilot.Extensions;
 
@@ -12,31 +13,30 @@ public static class AddSwaggerExtension
             c.EnableAnnotations();
             // c.SwaggerDoc("v1", new() { Title = "Minha API", Version = "v1" });
             //
-            // c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-            // {
-            //     Name = "Authorization",
-            //     Type = SecuritySchemeType.Http,
-            //     Scheme = "bearer",
-            //     BearerFormat = "JWT",
-            //     In = ParameterLocation.Header,
-            //     Description = "Informe o token JWT no formato: Bearer {seu_token}"
-            // });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Informe o token JWT no formato: Bearer {seu_token}"
+            });
 
-            // c.AddSecurityRequirement(document =>
-            //     new OpenApiSecurityRequirement
-            //     {
-            //         {
-            //             new OpenApiSecurityScheme
-            //             {
-            //                 Reference = new OpenApiReference
-            //                 {
-            //                     Type = ReferenceType.SecurityScheme,
-            //                     Id = "Bearer"
-            //                 }
-            //             },
-            //             Array.Empty<string>()
-            //         }
-            //     });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         });
 
 
